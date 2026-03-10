eb6baa9cbe1e017730bd8dc6e31c26ee18ea4f49
surpurkarsanjananarendra <sanjana.surpurkar@coditas.com>
2026-03-10 14:06:52 +0530

    Basic Changes

--- src/app/authentication/commons/constants/errors.go
+++ src/app/authentication/commons/constants/errors.go
@@ -61,6 +61,5 @@ const (
 const (
 	IncorrectOTPError = "Incorrect OTP"
 	OtpExpiredError   = "OTP Expired"
-	OtpMismatchError  = "OTP Mismatched"
-	SigninFailedError = "Signin Failed Using OTP"
+	SigninFailedError = "Signin Failed"
 )

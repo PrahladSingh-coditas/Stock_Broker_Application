e2129418618fbc905eba559c89833c9f595fd7d1
Sharanaabasppa Halle <135137499+Sharanu26@users.noreply.github.com>
2026-03-18 15:06:37 +0530

    Delete src/app/authentication/commons/constants/routes.go

--- src/app/authentication/commons/constants/routes.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const SwaggerRoute = "/swagger/*any"
-const AuthRoutePrefix = "/api/auth"
-const Signup = "/signup"
-const Signin = "/signin"
-const ValidateOtp = "/validate-otp"
-const ChangePassword = "/reset-password"

b1cc0f6e0ddf1d9ebdea43503e261b4e0a10aca0
Sharanaabasppa Halle <135137499+Sharanu26@users.noreply.github.com>
2026-03-18 20:37:01 +0530

    Delete src/app/authentication/models/changePasswordModel.go

--- src/app/authentication/models/changePasswordModel.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type BFFResetPasswordRequest struct {
-	Password        string `json:"password" validate:"required,min=8"`
-	ConfirmPassword string `json:"confirm_password" validate:"required"`
-}
